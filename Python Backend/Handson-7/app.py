6293956c5e029d62957d5ccbc2cbdff8f6587fc0
Sri Shanth <srishanth244@gmail.com>
2026-07-01 18:42:15 +0530

    Added Hands-On 7 - Flask CRUD API

---
+++ Python Backend/Handson-7/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from extensions import db
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object("config.Config")
+
+    db.init_app(app)
+
+    from courses import models
+
+    Migrate(app, db)
+
+    from courses.routes import courses_bp
+    app.register_blueprint(courses_bp)
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
