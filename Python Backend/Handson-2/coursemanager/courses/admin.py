a677cdf7e4562a7db7ff2a2ae1def10aef976d78
Sri Shanth <srishanth244@gmail.com>
2026-07-01 16:52:10 +0530

    Completed Python Backend Handson-2

---
+++ Python Backend/Handson-2/coursemanager/courses/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import Department, Course, Student, Enrollment
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "head_of_dept", "budget")
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "credits", "department")
+    search_fields = ("name", "code")
+    list_filter = ("department",)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "department",
+        "enrollment_year",
+    )
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("department", "enrollment_year")
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "course",
+        "enrollment_date",
+        "grade",
+    )
+    list_filter = ("course", "grade")
